19f9ea8f58b8ef17d58bdf832b0c50647f2f58b3
P.HungTham <hunggtham@gmail.com>
2023-10-27 17:43:06 +0900

    finish

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function addTask() {
     task.appendChild(xIcon);
   }
   inputBox.value = "";
+  saveData();
 }
 
 listContainer.addEventListener(
@@ -21,9 +22,18 @@ listContainer.addEventListener(
   function (e) {
     if (e.target.tagName === "LI") {
       e.target.classList.toggle("checked");
+      saveData();
     } else if (e.target.tagName === "SPAN") {
       e.target.parentElement.remove();
+      saveData();
     }
   },
   false
 );
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
